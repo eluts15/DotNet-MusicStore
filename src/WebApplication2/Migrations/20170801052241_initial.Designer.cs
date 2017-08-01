3a44eabe89ad4e3f505a83da88a2aa4600d8f640
eluts <eluts15@gmai.com>
2017-07-31 22:42:33 -0700

    music store

---
+++ src/WebApplication2/Migrations/20170801052241_initial.Designer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using WebApplication2.Models;
+
+namespace WebApplication2.Migrations
+{
+    [DbContext(typeof(AlbumContext))]
+    [Migration("20170801052241_initial")]
+    partial class initial
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.0.1")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("WebApplication2.Models.Album", b =>
+                {
+                    b.Property<int>("ID")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("AlbumName");
+
+                    b.Property<string>("Artist");
+
+                    b.Property<string>("Genre");
+
+                    b.Property<decimal>("Price");
+
+                    b.Property<DateTime>("ReleaseDate");
+
+                    b.HasKey("ID");
+
+                    b.ToTable("Album");
+                });
+        }
+    }
+}
---
+++ src/WebApplication2/Models/AlbumContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication2.Models
+{
+    public class AlbumContext : DbContext
+    {
+        public AlbumContext (DbContextOptions<AlbumContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Album> Album { get; set; }
+    }
+}
